26d3360fc2f839d6a7e7c392565408c54ace87e9
Kostiantyn Romanchuk <romanchuk.konstantin@gmail.com>
2024-03-10 16:35:01 +0100

    create

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import logging
 import asyncio
 from aiopath import AsyncPath
 from aioshutil import copyfile
+
+async def main():
+    apath = AsyncPath("hello.txt")
+    if await apath.exists():
+        new_path = AsyncPath('logs')
+        await new_path.mkdir(exist_ok=True, parents=True)
+        await copyfile(apath, new_path / apath)
+
+if __name__ == '__main__':
+    asyncio.run(main())
