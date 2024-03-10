6a3a779fc55adbbb21cdfc5c9baeb8684ab489f1
Kostiantyn Romanchuk <romanchuk.konstantin@gmail.com>
2024-03-10 16:36:49 +0100

    create

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import asyncio
 from aiopath import AsyncPath
 from aioshutil import copyfile
 
+parser = argparse.ArgumentParser(description="Sorting files")
+parser.add_argument("--source", "-s", required=True, help="Source dir")
+parser.add_argument("--output", "-o", help="Output dir", default="destination")
+args = vars(parser.parse_args())
+
+source = AsyncPath(args["source"])
+output = AsyncPath(args["output"])
+
 async def main():
     apath = AsyncPath("hello.txt")
     if await apath.exists():
